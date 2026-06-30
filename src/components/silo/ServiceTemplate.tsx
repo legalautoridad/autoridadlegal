255a4fc70bfdae8d37dadfcedb5cb8c55279ec74
Domingo Imperatori <d.imperatori.bou@gmail.com>
2026-06-30 07:40:06 +0200

    chore: clear stale lead data and update ServiceTemplate component logic

--- src/components/silo/ServiceTemplate.tsx
+++ src/components/silo/ServiceTemplate.tsx
@@ -286,7 +286,7 @@ export default async function ServiceTemplate({ service, city }: ServiceTemplate
                         aria-label="Hablar con el Asistente IA"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="shrink-0"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                        Asistente IA
+                        Web Asistente IA
                     </a>
                 </div>
             </div>
